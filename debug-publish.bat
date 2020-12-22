dc533538e2390235704ef36189157fc1ffd57f30
Al Iczin <al@iczin.guru>
2020-12-22 16:31:09 +0300

    TestApp

--- debug-publish.bat
+++ debug-publish.bat
@@ -3,12 +3,18 @@
 echo "Local FULL publish for each platform, with testing script-engine (not xunit) by 1testrunner and archive artifacts"
 
 call dotnet clean src\oscript\oscript.csproj
+call dotnet clean src\TestApp\TestApp.csproj
 
 call rmdir distrs /s /q
 
 echo {  "sdk": {    "version": "3.1.403"  } } > global.json
 
-call dotnet publish -p:PublishTrimmed=false -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x64\bin
+call dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\TestApp\TestApp.csproj -o distrs\net31\win-x64\bin
+call dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x64\bin
+
+call dotnet publish -p:PublishTrimmed=false -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x86  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\TestApp\TestApp.csproj -o distrs\net31\win-x86\bin
 call dotnet publish -p:PublishTrimmed=false -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x86  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x86\bin
+
+rem Под MаcOS/Linux TestApp не соберется - нет пакета для WPF
 call dotnet publish -p:PublishTrimmed=false -f netcoreapp3.1 -p:DebugType=None -r linux-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\linux-x64\bin
 call dotnet publish -p:PublishTrimmed=false -f netcoreapp3.1 -p:DebugType=None -r osx-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\osx-x64\bin
